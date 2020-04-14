efc5c655baf9e67e904f56e63b3ddb3eb91b9f4d
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-14 08:43:19 +0530

    Add App - link created to app

--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'articles',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'djangonautic.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates'], #add templates to my project
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- djangonautic/djangonautic/views.py
+++ djangonautic/djangonautic/views.py
@@ -8,4 +8,4 @@ def homepage(request):
 
 def about(request):
     #return HttpResponse("about")
-    return render(request,"about.html")
+    return render(request,"about.html") 
