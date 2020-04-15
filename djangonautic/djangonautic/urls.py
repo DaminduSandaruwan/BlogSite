8814f72b3afb460cd3ca99187050cb0b3ebb9b42
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-15 23:43:53 +0530

    use static files

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -2,6 +2,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from .import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     url(r'^admin/',admin.site.urls),
@@ -10,3 +12,5 @@ urlpatterns = [
     url(r'^$',views.homepage),
 
 ]
+
+urlpatterns += staticfiles_urlpatterns()
