182463a1cc4e757325a5add0f37faa0cba814444
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-16 22:23:14 +0530

    Uploading Media

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -6,8 +6,9 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png',blank=True )
 
-    # add in thumbnail later
+
     # add in author later
 
     def __str__(self):
--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 ) 
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
