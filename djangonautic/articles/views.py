78578e5be1f4f921c6b2270aa6e8d015e6fe0224
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-17 22:52:36 +0530

    Requiring login - only go create page when logged

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def article_list(request):
@@ -11,3 +12,7 @@ def article_detail(request, slug):
     #return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html',{'article':article})
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
