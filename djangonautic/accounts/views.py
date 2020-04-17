6210ae403b45147329e21f2005d5460c97c26598
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-17 22:00:56 +0530

    Login User in

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_view(request):
     if request.method =='POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             #log the user in
+            login(request,user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -19,6 +21,8 @@ def login_view(request):
         form = AuthenticationForm(data = request.POST)
         if form.is_valid():
             #log in the user
+            user = form.get_user()
+            login(request,user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
