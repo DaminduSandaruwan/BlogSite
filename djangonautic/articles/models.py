e3eb118ae7ebdbac329d10a8a24bbe083edb4413
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-04-18 00:06:45 +0530

    Create foreign key field - for back end

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Article(models.Model):
@@ -6,7 +7,8 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    thumb = models.ImageField(default='default.png',blank=True )
+    thumb = models.ImageField(default='default.png',blank=True)
+    author = models.ForeignKey(User, default=None,on_delete=models.CASCADE)
 
 
     # add in author later
